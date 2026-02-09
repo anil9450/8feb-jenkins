f2346474b94a0a6b31d4ade9d60d2619ed62a9bf
ANIL KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2026-02-09 21:26:55 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class first {
     public static void main(String[] args) {
         // The line below prints the message to the console
         System.out.println("Hello, World!");
